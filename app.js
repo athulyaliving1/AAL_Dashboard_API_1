34515894703a33e0343103a226071d68068442db
athulyaliving1 <muthukumar@athulyaliving.com>
2023-11-27 16:08:03 +0530

    FIX:portfix

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -50,12 +50,10 @@ app.use(function(err, req, res, next) {
 
 
 
+var PORT = 4080;
 
 
 
-
-
-const PORT = process.env.PORT || 4080;
 
 
 app.listen(PORT, () => {
